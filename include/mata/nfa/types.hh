5b4a64e54a219cbba187c3664ac6e53cb18c1cd3
David Chocholatý <chocholaty.david@protonmail.com>
2024-11-20 12:31:18 +0100

    feat(cntnfa): Add constructors for `CounterState` and `CounterStateSet` classes

--- include/mata/nfa/types.hh
+++ include/mata/nfa/types.hh
@@ -19,28 +19,32 @@ extern const std::string TYPE_NFA;
 using State = unsigned long;
 using StateSet = mata::utils::OrdVector<State>;
 
-/// State with counter (State state and void* counter_ptr).
+/// State with a counter (@c State @c state and @c void* @c counter_ptr).
 struct CounterState {
     State state; ///< Automaton state.
     void* counter_ptr; ///< Pointer to the counter table when transitioning to a state.
 
     CounterState() : state(), counter_ptr(nullptr) {}
+
     CounterState(const CounterState&) = default;
     CounterState(CounterState&&) = default;
-    CounterState &operator=(const CounterState&) = default;
-    CounterState &operator=(CounterState&&) = default;
-    CounterState(State state, void* counter_ptr = nullptr) : state(state), counter_ptr(counter_ptr) {}
+    CounterState& operator=(const CounterState&) = default;
+    CounterState& operator=(CounterState&&) = default;
 
+    CounterState(const State& state): state{ state }, counter_ptr(nullptr) {} // NOLINT(*-explicit-constructor)
+    CounterState(State&& state): state{ state }, counter_ptr(nullptr) {} // NOLINT(*-explicit-constructor)
+    CounterState& operator=(const State& other) { state = other; return *this; }
+    CounterState& operator=(State&& other) { state = other; return *this; }
+
+    CounterState(const State state, void* counter_ptr) : state(state), counter_ptr(counter_ptr) {} // NOLINT(*-explicit-constructor)
+
+    auto operator<=>(const State& other) const { return state <=> other; }
+    bool operator==(const State other) const { return state == other; }
     auto operator<=>(const CounterState&) const = default;
+    bool operator==(const CounterState& other) const { return state == other; }
 
-    bool operator==(State other) const { return state == other; }
-    bool operator!=(State other) const { return state != other; }
-    bool operator<(State other) const { return state < other; }
-    bool operator<=(State other) const { return state <= other; }
-    bool operator>(State other) const { return state > other; }
-    bool operator>=(State other) const { return state >= other; }
 
-    operator State() const { return state; }
+    operator State() const { return state; } // NOLINT(*-explicit-constructor)
 };
 
 /// TODO: Can be very slow! Change this later.
@@ -48,17 +52,40 @@ class CounterStateSet : public mata::utils::OrdVector<CounterState> {
 public:
     CounterStateSet() = default;
 
-    explicit CounterStateSet(State state) {
+    CounterStateSet(State state) { // NOLINT(*-explicit-constructor)
         this->push_back(CounterState(state));
     }
-
-    explicit CounterStateSet(const StateSet& state_set) {
-        for (const State& state : state_set) {
+    CounterStateSet(StateSet& state_set) { // NOLINT(*-explicit-constructor)
+        for (const State& state: state_set) {
             this->push_back(state);
         }
     }
+    CounterStateSet(StateSet&& state_set) { // NOLINT(*-explicit-constructor)
+        for (const State& state: state_set) {
+            this->push_back(state);
+        }
+    }
+    CounterStateSet& operator=(const StateSet& state_set) {
+        for (const State& state: state_set) {
+            this->push_back(state);
+        }
+        return *this;
+    }
+    CounterStateSet& operator=(StateSet&& state_set) {
+        for (const State& state: state_set) {
+            this->push_back(state);
+        }
+        return *this;
+    }
+    CounterStateSet(const CounterStateSet& counter_state_set) = default;
+    CounterStateSet(CounterStateSet&& counter_state_set) noexcept = default;
+    CounterStateSet& operator=(const CounterStateSet& counter_state_set) = default;
+    CounterStateSet& operator=(CounterStateSet&& counter_state_set) noexcept = default;
 
-    operator StateSet() const {
+    // FIXME: This is severely limiting. Basically, this cannot ever be used in production unless explicitly requested.
+    //  Should this be explicit? Probably no, but some iteration over CounterStateSet which would behave as a StateSet would be good.
+    //  But that is already happening, no?
+    operator StateSet() const { // NOLINT(*-explicit-constructor)
         StateSet state_set;
         for (const auto& target : *this) {
             state_set.push_back(target.state);
